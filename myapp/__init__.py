c9e8d681d3b49db94c786d2fdac7512b7a72307b
zackbeckwith <z.beckwith99@gmail.com>
2022-04-13 16:20:53 -0400

    deploy

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -19,7 +19,7 @@ app.config['UPLOADED_PHOTOS_DEST'] = 'myapp/static'
 
 
 # set up connection to db
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/finatics"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/finatics"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
